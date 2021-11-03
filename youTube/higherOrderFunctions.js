7815fe488fa410f4247f0762ad29ecf386ec0a56
JSmith604 <jacquelinesmith190@gmail.com>
2021-11-03 19:58:17 +0000

    map and arrow function practice

--- youTube/higherOrderFunctions.js
+++ youTube/higherOrderFunctions.js
@@ -1,16 +1,16 @@
-const companies= [
-  {name: "Company One", category: "Finance", start: 1981, end: 2004},
-  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-  {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-];
+// const companies= [
+//   {name: "Company One", category: "Finance", start: 1981, end: 2004},
+//   {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+//   {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+//   {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+//   {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+//   {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+//   {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+//   {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+//   {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+// ];
 
-// const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // for(let i = 0; companies.length; i++) {
 //   console.log(companies[i]);
@@ -63,7 +63,10 @@ const companies= [
 
 //With ES 6 Arrow Function Syntax
 
+//company is the parameter and what comes after the arrow is the condition, the arrow is used instead of the word function and the condition after the arrow is used instead of an if statement
+
 // const retailCompanies = companies.filter(company => company.category === 'Retail');
+
 
 
 // console.log(retailCompanies);
@@ -90,15 +93,30 @@ const companies= [
 
 // console.log(companyNames);
 
-// const testMap = companies.map(function(company) {
-//   return `${company.name} [${company.start} - ${company.end}]`;
-// });
-
-// console.log(testMap);
-
+//Can try adding anything to map
 
 // const testMap = companies.map(function(company) {
 //   return 1;
 // });
 
 // console.log(testMap);
+
+//Map using more variables, making an array of strings with multiple variables
+
+// const testMap = companies.map(function(company) {
+//   return `${company.name} [${company.start} - ${company.end}]`;
+// });
+
+// console.log(testMap);
+
+// const testMap = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
+
+// console.log(testMap);
+
+// const agesSquare = ages.map(age => Math.sqrt(age));
+
+// console.log(agesSquare);
+
+const agesTimesTwo = ages.map(age => age * 2);
+
+console.log(agesTimesTwo);
