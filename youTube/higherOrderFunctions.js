e2ad15ad88c8d9aab5223e2e04c7bd4ae1fa0f75
JSmith604 <jacquelinesmith190@gmail.com>
2021-11-03 18:34:31 +0000

    add filter and arrow functioin

--- youTube/higherOrderFunctions.js
+++ youTube/higherOrderFunctions.js
@@ -24,11 +24,23 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 //filter, allows us to filter things out from the array
 
-let canDrink = [];
-for(let i = 0; i < ages.length; i++) {
-  if(ages[i] >= 21) {
-    canDrink.push(ages[i]);
-  }
-}
+// let canDrink = [];
+// for(let i = 0; i < ages.length; i++) {
+//   if(ages[i] >= 21) {
+//     canDrink.push(ages[i]);
+//   }
+// }
+
+// console.log(canDrink);
+
+// const canDrink = ages.filter(function(age) {
+//   if(age >= 21) {
+//     return true;
+//   }
+// })
+
+// console.log(canDrink);
+
+const canDrink = ages.filter(age=> age >= 21);
 
 console.log(canDrink);
